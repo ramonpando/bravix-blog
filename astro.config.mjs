5d5d293ab7e9a44266b618bbaaf97283d52f5502
root <root@srv899535.hstgr.cloud>
2025-09-25 01:04:15 +0000

    Initial Astro blog setup for /blog path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://bravix.com.mx',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
